f6ba294bc9b2791a48014c3e27fe31839fdc2aad
DimosMellios <65303469+DimosMellios@users.noreply.github.com>
2020-06-30 00:40:52 +0200

    Add files via upload

---
+++ Visuals/TSNE.py
@@ -0,0 +1,38 @@
+#IMPORTS
+import numpy as np
+from sklearn.manifold import TSNE
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+
+
+def scatter(x, labels, title):
+    '''Creates the scatter-plot for the TSNE manifold'''
+    
+    # Pick a color for the different embeddings.
+    colors = len(np.unique(labels))
+    palette = np.array(sns.color_palette("hls", colors))
+
+    # We create a scatter plot.
+    f = plt.figure(figsize=(8, 8))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(x[:,0], x[:,1], lw=0, s=40,
+                    c=palette[labels.astype(np.int)])
+    plt.xlim(-25, 25)
+    plt.ylim(-25, 25)
+    ax.axis('off')
+    ax.axis('tight')
+
+    # We add the labels for each digit.
+    txts = []
+    for i in range(10):
+        # Position of each label.
+        xtext, ytext = np.median(x[labels == i, :], axis=0)
+        txt = ax.text(xtext, ytext, str(i), fontsize=24)
+        txt.set_path_effects([
+            PathEffects.Stroke(linewidth=5, foreground="w"),
+            PathEffects.Normal()])
+        txts.append(txt)
+        
+    plt.suptitle(title)
+
